dabf785a5de44401951e220919bc84f0a28db044
yj0216 <yeojun0216@naver.com>
2023-04-06 21:15:59 +0900

    js idbox

--- coopang/js/register.js
+++ coopang/js/register.js
@@ -2,20 +2,41 @@ const iconNum = ["#register-icon1","#register-icon2","#register-icon3","#registe
 const idForm = document.querySelector(".register-form-first");
 const idInput = document.querySelector(".register-input");
 const id = idInput.querySelector("input");
+const BORDER_BOTTOM_RED = "border-bottom-red";
+const BORDER_BOTTOM_BLUE = "border-bottom-blue";
+const ADD_MESSAGE = "add-message";
+
 
 function handleBlueBottom(){
-    const span = document.querySelector(iconNum[0]);
-    const inputBox = idInput;
-    span.classList.add("border-bottom-blue");
-    inputBox.classList.add("border-bottom-blue");
+        const warningMessage = idForm.querySelector(".add-message");
+        const span = document.querySelector(iconNum[0]);
+        const inputBox = idInput;
+        if(warningMessage)idForm.removeChild(warningMessage);
+        span.classList.add(BORDER_BOTTOM_BLUE);
+        inputBox.classList.add(BORDER_BOTTOM_BLUE);      
+        document.addEventListener("click",handleRedBottom);
 }
 
-function handleRedBottom(){
+function handleRedBottom(event){
     console.log("작동");
-    const span = document.querySelector(iconNum[0]);
-    const inputBox = idInput;
-    span.classList.add("border-red-blue");
-    inputBox.classList.add("border-red-blue");
+    const warningMessage = idForm.querySelector(".add-message");
+    if(id.value.length > 8)
+    {
+        if(warningMessage)idForm.removeChild(warningMessage);
+        handleBlueBottom();
+    }else if(event.target !== id && idForm.childElementCount === 2)
+    {
+            const warningMessage = document.createElement("div");
+            const span = document.querySelector(iconNum[0]);
+            const inputBox = idInput;
+            warningMessage.innerText = "이메일을 입력하세요";
+            warningMessage.classList.add(ADD_MESSAGE);
+            span.classList.remove(BORDER_BOTTOM_BLUE);
+            inputBox.classList.remove(BORDER_BOTTOM_BLUE);
+            span.classList.add(BORDER_BOTTOM_RED);
+            inputBox.classList.add(BORDER_BOTTOM_RED);
+            idForm.appendChild(warningMessage);            
+    }
+
 }
-id.addEventListener("click",handleBlueBottom)
-id.addEventListener("click mouseout",handleRedBottom)
+id.addEventListener("click",handleBlueBottom);
