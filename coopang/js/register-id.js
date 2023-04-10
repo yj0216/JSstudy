d79589f8c4c0d0a8f097b8acc78b2c7a10a950ef
yj0216 <yeojun0216@naver.com>
2023-04-10 20:30:40 +0900

    id js완성

--- coopang/js/register-id.js
+++ coopang/js/register-id.js
@@ -1,42 +1,53 @@
-const iconNum = ["#register-icon1","#register-icon2","#register-icon3","#register-icon4","#register-icon5"];
+const iconNum = "#register-icon1";
 const idForm = document.querySelector(".register-form-first");
 const idInput = document.querySelector(".register-input");
 const id = idInput.querySelector("input");
 const BORDER_BOTTOM_RED = "border-bottom-red";
 const BORDER_BOTTOM_BLUE = "border-bottom-blue";
+const INPUT_SUCCESS = "input-success"; 
 const ADD_MESSAGE = "add-message";
 
 
 function handleBlueBottom(){
-        const warningMessage = idForm.querySelector(".add-message");
-        const span = document.querySelector(iconNum[0]);
+        const warningDiv = idForm.querySelector("div");
+        const span = document.querySelector(iconNum);
         const inputBox = idInput;
-        if(warningMessage)idForm.removeChild(warningMessage);
+        if(warningDiv)idForm.removeChild(warningDiv);
         span.classList.add(BORDER_BOTTOM_BLUE);
         inputBox.classList.add(BORDER_BOTTOM_BLUE);      
-        document.addEventListener("click",handleRedBottom);
+        document.addEventListener("click",handlefailBottom);
 }
 
-function handleRedBottom(event){
+function handlefailBottom(event){
     console.log("작동");
-    const warningMessage = idForm.querySelector(".add-message");
+    const warningDiv = idForm.querySelector("div");
     if(id.value.length > 8)
     {
-        if(warningMessage)idForm.removeChild(warningMessage);
-        handleBlueBottom();
+        if(warningDiv)warningDiv.removeChild(warningDiv);
+        handleSuccessBottom();
     }else if(event.target !== id && idForm.childElementCount === 2)
     {
+            const div2 = document.createElement("div");
             const warningMessage = document.createElement("div");
-            const span = document.querySelector(iconNum[0]);
+            const span = document.querySelector(iconNum);
             const inputBox = idInput;
+            const input = id;
             warningMessage.innerText = "이메일을 입력하세요";
             warningMessage.classList.add(ADD_MESSAGE);
             span.classList.remove(BORDER_BOTTOM_BLUE);
             inputBox.classList.remove(BORDER_BOTTOM_BLUE);
+            input.classList.remove(INPUT_SUCCESS);
             span.classList.add(BORDER_BOTTOM_RED);
             inputBox.classList.add(BORDER_BOTTOM_RED);
-            idForm.appendChild(warningMessage);            
+            div2.appendChild(warningMessage);
+            idForm.appendChild(div2);            
     }
-
 }
+function handleSuccessBottom(){
+        const warningMessage = idForm.querySelector(".add-message");
+        const inputBox = id;
+        if(warningMessage)idForm.removeChild(warningMessage);
+        inputBox.classList.add(INPUT_SUCCESS);      
+}
+
 id.addEventListener("click",handleBlueBottom);
